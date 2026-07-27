9527c07ccae5620775e8f05a5d596c6ee5e143fe
SajedulIslam2312027 <2312027@iub.edu.bd>
2026-07-28 01:08:02 +0600

    usg3completed

--- src/main/java/com/example/simulatingoperationsofarentacarcompany_final/Sajedul/U2g3_VehicleStatusUpdateController.java
+++ src/main/java/com/example/simulatingoperationsofarentacarcompany_final/Sajedul/U2g3_VehicleStatusUpdateController.java
@@ -6,24 +6,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class U2g2_MaintenanceSchedulerController {
+public class U2g3_VehicleStatusUpdateController {
 
     @javafx.fxml.FXML
-    private ComboBox typeCB;
-    @javafx.fxml.FXML
-    private DatePicker serviceDateDP;
+    private ComboBox statusCB;
     @javafx.fxml.FXML
     private TextField vehicleIdTF;
+    @javafx.fxml.FXML
+    private TextArea reasonTA;
 
     @javafx.fxml.FXML
-    public void scheduleButtonOnAction(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sajedul/U2g3_VehicleStatusUpdate.fxml"));
+    public void updateStatusButtonOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sajedul/U2g4_FleetTracking.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         Stage nextStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         nextStage.setTitle("Rent A Car");
